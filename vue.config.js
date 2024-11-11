c0e6b519de913afe8b4fddf7e3d8fc9743de4ece
willsondev <willsondev12@gmail.com>
2024-11-11 00:23:57 -0300

    jk

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-   pwa: {
-    name: 'Mi Aplicación Vue',
-    themeColor: '#42b983',
-    msTileColor: '#ffffff',
-    manifestOptions: {
-      background_color: '#ffffff',
-      start_url: '/',
-      display: 'standalone'
-    },
-    workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true,
-    }
-  }
+   
 });
